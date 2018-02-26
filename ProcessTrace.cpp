db62dc1df18fc674858d71e89800e58e0e81e4c9
maxwellthefourth <maxwellthefourth@maxwellthefourth-VirtualBox>
2018-02-26 16:21:56 -0700

    exception handling

--- ProcessTrace.cpp
+++ ProcessTrace.cpp
@@ -46,83 +46,101 @@ void ProcessTrace::Execute() {
         if (tempLine.at(0) != '#') {
             std::istringstream iss(tempLine);
             while (iss >> tempWord) { // Look at the first word of the line
-                if (tempWord == "alloc") {
-                    uint32_t vaddr, size, paddr;
-                    iss >> std::hex >> vaddr;
-                    iss >> std::hex >> size;
-                    numPages = size;
-                    // allocate memory somehow
-                    memory.ToPhysical(vaddr, paddr, true);
-                    memory.put_bytes(paddr, size, 0); // New allocated memory is initialized to 0
-                } else if (tempWord == "compare") {
-                    uint32_t addr, expected_values;
-                    iss >> std::hex >> addr;
-                    while (iss >> expected_values) { // Goes through each expected value in the line
-                        uint8_t data[1]; // Create a temp array of size 1 and store byte in it, and then is compared one by one
-                        memory.get_byte(data, addr);
-                        if (data[0] != expected_values) {
-                            cout << "compare error at address " << std::hex << addr << ", expected " << std::hex << expected_values << ", actual " << std::hex << unsigned(data[0]) << endl;
+                uint32_t address;
+                try { // try catch to find exceptions
+                    iss >> std::hex >> address;
+                    if (tempWord == "alloc") {
+                        uint32_t vaddr, size, paddr;
+                        vaddr = address;
+                        iss >> std::hex >> size;
+                        numPages = size;
+                        // allocate memory somehow
+                        memory.ToPhysical(vaddr, paddr, true);
+                        memory.put_bytes(paddr, size, 0); // New allocated memory is initialized to 0
+                    } else if (tempWord == "compare") {
+                        uint32_t addr, expected_values;
+                        addr = address;
+                        while (iss >> expected_values) { // Goes through each expected value in the line
+                            uint8_t data[1]; // Create a temp array of size 1 and store byte in it, and then is compared one by one
+                            memory.get_byte(data, addr);
+                            if (data[0] != expected_values) {
+                                cout << "compare error at address " << std::hex << addr << ", expected " << std::hex << expected_values << ", actual " << std::hex << unsigned(data[0]) << endl;
+                            }
+                            addr++;
                         }
-                        addr++;
-                    }
-                } else if (tempWord == "put") {
-                    uint32_t addr, val;
-                    uint8_t data[1];
-                    iss >> std::hex >> addr;
-                    while (iss >> val) {
-                        data[0] = val; // Puts value into an array of size 1 and then that array is put into the specified address
-                        memory.put_byte(addr, data); // Uses MMU.h file to store bytes
-                        addr++;
-                    }
-                } else if (tempWord == "fill") {
-                    uint32_t addr, count, val;
-                    iss >> std::hex >> addr; // First three values are the address, the amount of numbers to put, and the value to be put in
-                    iss >> std::hex >> count;
-                    iss >> std::hex >> val;
-                    uint8_t data[1];
-                    for (int i = 0; i < count; i++) {
-                        data[0] = val; // Does the same as 'put', except loops for however many times is specified
-                        memory.put_byte(addr, data); // Uses MMU.h file to store bytes
-                        addr++;
-                    }
-                } else if (tempWord == "copy") {
-                    uint32_t dest_addr, src_addr, count;
-                    iss >> std::hex >> dest_addr; // First three values are the destination address, the source address, and the amount of numbers to copy
-                    iss >> std::hex >> src_addr;
-                    iss >> std::hex >> count;
-                    uint8_t data[1];
-                    for (int i = 0; i < count; i++) {
-                        memory.get_byte(data, src_addr); // Uses MMU.h file to get bytes...
-                        memory.put_byte(dest_addr, data); // ...which is then stored again using the MMU.h file
-                        src_addr++;
-                        dest_addr++;
-                    }
-                } else if (tempWord == "dump") {
-                    uint32_t addr, count;
-                    iss >> std::hex >> addr; // First two values are the address and the amount to dump
-                    iss >> std::hex >> count;
-                    cout << addr << endl;
-                    int i = 0;
-                    uint8_t data[1];
-                    for (; i < count; i++) { // For loop to go through each value starting from address
-                        memory.get_byte(data, addr);
-                        cout << " " << std::setw(2) << std::setfill('0') << std::hex << unsigned(data[0]);
-                        if (i % 16 == 15) // New line every 16 values
+                    } else if (tempWord == "put") {
+                        uint32_t addr, val;
+                        uint8_t data[1];
+                        addr = address;
+                        while (iss >> val) {
+                            data[0] = val; // Puts value into an array of size 1 and then that array is put into the specified address
+                            memory.put_byte(addr, data); // Uses MMU.h file to store bytes
+                            addr++;
+                        }
+                    } else if (tempWord == "fill") {
+                        uint32_t addr, count, val;
+                        addr = address; // First three values are the address, the amount of numbers to put, and the value to be put in
+                        iss >> std::hex >> count;
+                        iss >> std::hex >> val;
+                        uint8_t data[1];
+                        for (int i = 0; i < count; i++) {
+                            data[0] = val; // Does the same as 'put', except loops for however many times is specified
+                            memory.put_byte(addr, data); // Uses MMU.h file to store bytes
+                            addr++;
+                        }
+                    } else if (tempWord == "copy") {
+                        uint32_t dest_addr, src_addr, count;
+                        dest_addr = address; // First three values are the destination address, the source address, and the amount of numbers to copy
+                        iss >> std::hex >> src_addr;
+                        iss >> std::hex >> count;
+                        uint8_t data[1];
+                        for (int i = 0; i < count; i++) {
+                            memory.get_byte(data, src_addr); // Uses MMU.h file to get bytes...
+                            memory.put_byte(dest_addr, data); // ...which is then stored again using the MMU.h file
+                            src_addr++;
+                            dest_addr++;
+                        }
+                    } else if (tempWord == "dump") {
+                        uint32_t addr, count;
+                        addr = address; // First two values are the address and the amount to dump
+                        iss >> std::hex >> count;
+                        cout << addr << endl;
+                        int i = 0;
+                        uint8_t data[1];
+                        for (; i < count; i++) { // For loop to go through each value starting from address
+                            memory.get_byte(data, addr);
+                            cout << " " << std::setw(2) << std::setfill('0') << std::hex << unsigned(data[0]);
+                            if (i % 16 == 15) // New line every 16 values
+                                cout << endl;
+                            addr++;
+                        }
+                        if (i % 16 != 0) // Only prints another line after the dump if it doesn't end at 16
                             cout << endl;
-                        addr++;
+                    } else if (tempWord == "writable") {
+                        uint32_t vaddr, size, status;
+                        vaddr = address;
+                        iss >> std::hex >> size;
+                        iss >> std::hex >> status;
+                        if (status == 0) {
+
+                        } else {
+
+                        }
                     }
-                    if (i % 16 != 0) // Only prints another line after the dump if it doesn't end at 16
-                        cout << endl;
-                } else if (tempWord == "writable") {
-                    uint32_t vaddr, size, status;
-                    iss >> std::hex >> vaddr;
-                    iss >> std::hex >> size;
-                    iss >> std::hex >> status;
-                    if (status == 0) {
-                        
-                    } else {
-                        
-                    }
+                } catch (mem::PageFaultException e1) {
+                    cout << "Exception: PageFaultException at " << address << endl;
+                    cout << "what() message: " << e1.what() << endl;
+                    mem::PMCB tempPMCB;
+                    memory.get_PMCB(tempPMCB);
+                    tempPMCB.operation_state = mem::PMCB::NONE;
+                    memory.set_PMCB(tempPMCB);
+                } catch (mem::WritePermissionFaultException e2) {
+                    cout << "Exception: WritePermissionFaultException at " << address << endl;
+                    cout << "what() message: " << e2.what() << endl;
+                    mem::PMCB tempPMCB;
+                    memory.get_PMCB(tempPMCB);
+                    tempPMCB.operation_state = mem::PMCB::NONE;
+                    memory.set_PMCB(tempPMCB);
                 }
             }
         }
